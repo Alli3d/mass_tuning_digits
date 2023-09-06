04e256c0b4e6416011d72322d56a5fe5a885a8e4
Alli Edwards <edwardsallistair15@gmail.com>
2023-09-06 17:20:53 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,60 @@
+import pandas as pd
+from sklearn.datasets import load_digits
+from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn import svm
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.naive_bayes import GaussianNB
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.tree import DecisionTreeClassifier
+
+df = load_digits()
+x_train, y_train, x_test, y_test = train_test_split(df.data, df.target, test_size=0.2)
+
+model_params = {
+    'svm': {
+        'model': svm.SVC(),
+        'params': {
+            'gamma': ['scale','auto'],
+            'kernel': ['rbf', 'linear']
+        }
+    },
+    'random_forest': {
+        'model': RandomForestClassifier(),
+        'params': {
+            'n_estimators': [1,5,10,50,100]
+       }
+    },
+    'logistc_regression' : {
+        'model': LogisticRegression(solver='liblinear', multi_class='auto'),
+        'params': {
+            'C': [1,5,10,50,100]
+        }
+    },
+    'gaussianNB': {
+        'model': GaussianNB(),
+        'params': {}
+    },
+    'multinomialNB': {
+        'model': MultinomialNB(),
+        'params': {}
+    },
+    'decisiontree': {
+        'model': DecisionTreeClassifier(),
+        'params': {}
+    }
+}
+
+scores = []
+
+for model_name, mp in model_params.items():
+    clf = GridSearchCV(mp['model'], mp['params'], cv=5, return_train_score=False)
+    clf.fit(df.data, df.target)
+    scores.append({
+        'model': model_name,
+        'best_score': clf.best_score_,
+        'best_params': clf.best_params_
+    })
+
+df = pd.DataFrame(scores, columns=['model', 'best_score', 'best_params'])
+print(df)
